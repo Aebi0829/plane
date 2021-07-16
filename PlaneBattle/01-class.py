8915ea90e857dfce68a69c57ecc92f5c6d7a59f4
unknown <leehai0829@gmail.com>
2021-07-16 15:05:36 +0800

    pygame   for study

---
+++ PlaneBattle/01-class.py
@@ -0,0 +1,18 @@
+"""
+飞机大战
+"""
+# TODO 1.背景类
+class Background:
+    pass
+# TODO 2.玩家类
+class PlayerPlane:
+    pass
+# TODO 3.敌机类
+class EnemyPlane:
+    pass
+# TODO 4.子弹类
+class Bullet:
+    pass
+# TODO 5.游戏类
+class Game:
+    pass
